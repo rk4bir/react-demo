b7005e6445beb8194070abdd8a6f9f178e98b1d5
rk4bir <r.kabir01@protonmail.com>
2020-05-06 13:08:03 +0600

    cleaned

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 //import GoogleApiWrapper from './Map.js';
 import Nav from './components/Nav.js';
 import LandingPage from './components/LandingPage.js';
+import MapPage from './components/MapPage.js';
 
 function App() {
   return (
@@ -12,6 +13,11 @@ function App() {
       <Nav />
       <Container className='PageContainer' maxWidth="lg">
       	<LandingPage />
+      </Container>
+
+      <Nav />
+      <Container className='PageContainer' maxWidth="lg">
+      	<MapPage />
       </Container>
     </div>
   );
